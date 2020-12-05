b20bf083f8060eaeddda474b4397c278da2e29ef
Gur Telem <gtelem@apple.com>
2020-12-05 12:39:44 +0200

    Update check coverage excludes and limit

--- check_coverage.sh
+++ check_coverage.sh
@@ -7,14 +7,14 @@ g++ -o main  -std=c++11 -DNDEBUG -fprofile-arcs -ftest-coverage ../unittests/tes
 ./main
 pwd
 gcov ./test_* > tmp.log
-grep -n -A2 -B1 -e "Xcode" -e "gtest" tmp.log | sed -n 's/^\([0-9]\{1,\}\).*/\1d/p' | sed -f /dev/stdin tmp.log > ../coverage.log
+grep -n -A2 -B1 -e "Xcode" -e "gtest" -e "/user/include" tmp.log | sed -n 's/^\([0-9]\{1,\}\).*/\1d/p' | sed -f /dev/stdin tmp.log > ../coverage.log
 cd ../
 rm -r coverage_tmp
 
 echo "Coverage:"
 cat coverage.log
 
-bad_count="$(cat coverage.log | grep % | cut -d % -f 1 | awk -F: '{if($2<99)print$2}' | wc -l)"
+bad_count="$(cat coverage.log | grep % | cut -d % -f 1 | awk -F: '{if($2<98.5)print$2}' | wc -l)"
 if [[ $bad_count -eq 0 ]]; then
     (exit 0);
 else
