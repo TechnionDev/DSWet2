d4fc14e4d27e12f86dee03ea98e5f1d72c0d51a0
Gur Telem <gtelem@apple.com>
2020-12-05 12:26:09 +0200

    Update ci/cd to check coverage over 99

--- check_coverage.sh
+++ check_coverage.sh
@@ -3,10 +3,21 @@
 set -e
 mkdir -p coverage_tmp
 cd coverage_tmp
-g++ -o main  -std=c++11 -fprofile-arcs -ftest-coverage ../unittests/test_*.cpp ../*.cpp -lgtest -lgtest_main
+g++ -o main  -std=c++11 -DNDEBUG -fprofile-arcs -ftest-coverage ../unittests/test_*.cpp ../*.cpp -lgtest -lgtest_main
 ./main
 pwd
 gcov ./test_* > tmp.log
 grep -n -A2 -B1 -e "Xcode" -e "gtest" tmp.log | sed -n 's/^\([0-9]\{1,\}\).*/\1d/p' | sed -f /dev/stdin tmp.log > ../coverage.log
 cd ../
 rm -r coverage_tmp
+
+echo "Coverage:"
+cat coverage.log
+
+bad_count="$(cat coverage.log | grep % | cut -d % -f 1 | awk -F: '{if($2<99)print$2}' | wc -l)"
+if [[ $bad_count -eq 0 ]]; then
+    (exit 0);
+else
+    echo "Insucfficient coverrage"
+    (exit 1);
+fi
