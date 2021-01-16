390f5d61fd9f5000f13ead946f7611e54492bf8b
Gur Telem <gur111telem@gmail.com>
2021-01-16 11:38:21 +0200

    Continue hasmap testing

--- unittests/test_HashMap.cpp
+++ unittests/test_HashMap.cpp
@@ -1,57 +1,66 @@
-#include <execinfo.h>
-#include <signal.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 #include <string>
 
 #include "helperTest.h"
 
-void handler(int sig) {
-    void *array[10];
-    size_t size;
-
-    // get void*'s for all entries on the stack
-    size = backtrace(array, 10);
-
-    // print out all the frames to stderr
-    fprintf(stderr, "Error: signal %d:\n", sig);
-    backtrace_symbols_fd(array, size, STDERR_FILENO);
-    exit(1);
-}
-
 TEST(HashMap, Basic) {
     // TEST_TIMEOUT_BEGIN;
-    HashMap<long long> &map = *(new HashMap<long long>());
+    HashMap<long long> map;
     ASSERT_TRUE(map.isEmpty());
     int key = 7896234;  // TODO: Change to 7896234
     map.set(key, 87);
     ASSERT_EQ(map[key], 87LL);
-
-    delete &map;
     // TODO: TEST_TIMEOUT_FAIL_END(TIME_UNIT * 15);
 }
 
-TEST(HashMap, MultipleAdds) {
-    // TEST_TIMEOUT_BEGIN;
-    signal(SIGSEGV, handler);  // install our handler
+TEST(HashMap, LotsOfAdds) {
+    TEST_TIMEOUT_BEGIN;
+    HashMap<long long> map;
+    for (int i = 0; i < COUNT * 10; i++) {
+        map.set(i, i * i);
+    }
 
-    cout << "Starts" << endl;
-    HashMap<long long> &map = *(new HashMap<long long>());
-    cout << "Test0" << endl;
+    for (int i = 0; i < COUNT; i += 7) {
+        ASSERT_EQ(map.get(i), i * i);
+    }
+
+    for (int i = 2; i < COUNT; i += 7) {
+        ASSERT_EQ(map[i], i * i);
+    }
+
+    TEST_TIMEOUT_FAIL_END(TIME_UNIT * COUNT);
+}
+
+TEST(HashMap, AddRemoveRepeat) {
+    TEST_TIMEOUT_BEGIN;
+    HashMap<long long> map;
+    for (int i = 0; i < COUNT * 10; i++) {
+        map.set(i, i * i);
+        ASSERT_EQ(map.get(i), i * i);
+        map.remove(i);
+        ASSERT_TRUE(map.isEmpty());
+    }
+
     ASSERT_TRUE(map.isEmpty());
-    int key = 7896234;
-    cout << "Test1" << endl;
-    map.set(key, key * 2);
-    cout << "Test2" << endl;
-    map.set(key * 7, key * 3);
-    cout << "Test3" << endl;
-    ASSERT_EQ(map[key], key * 2);
-    cout << "Test4" << endl;
-    ASSERT_EQ(map[key * 7], key * 3);
-    cout << "Test5" << endl;
 
-    delete &map;
-    // TODO: TEST_TIMEOUT_FAIL_END(TIME_UNIT * 15);
+    TEST_TIMEOUT_FAIL_END(TIME_UNIT * COUNT);
+}
+
+TEST(HashMap, AddThenRemove) {
+    // TEST_TIMEOUT_BEGIN;
+    HashMap<long long> map;
+    for (int i = 0; i < COUNT * 10; i++) {
+        map.set(i, i * i);
+        ASSERT_EQ(map.get(i), i * i);
+    }
+
+    for (int i = 0; i < COUNT * 10; i++) {
+        map.remove(i);
+        ASSERT_ANY_THROW(map.get(i));
+    }
+
+    ASSERT_TRUE(map.isEmpty());
+
+    // TEST_TIMEOUT_FAIL_END(TIME_UNIT * COUNT);
 }
